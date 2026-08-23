62943efe85bf50fe82f39f0cec95e3eb7df44afd
AnmolGupta912 <pfyplannedforu@gmail.com>
2026-08-23 11:46:17 +0530

    Setup Complete

--- src/app.js
+++ src/app.js
@@ -9,5 +9,12 @@ app.use(express.json({limit: "16kb"}))
 app.use(express.urlencoded({extended: true, limit: "16kb"}))
 app.use(express.static("public"))
 
+// routes import
+
+
+
+// routes declearation
+
+
 
 export {app, port}
--- src/db/index.js
+++ src/db/index.js
@@ -4,7 +4,8 @@ import { DB_NAME } from "../constands.js";
 const connentDB = async () => {
     try {
         const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}${DB_NAME}`)
-        console.log("MongoDB connect: Host: ",connectionInstance)
+        // console.log("MongoDB connect: Host: ",connectionInstance)
+
     } catch (err) {
         console.log("MongoDb connenttion failed: ", err)
         process.exit(1)
