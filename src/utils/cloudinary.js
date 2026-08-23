356589e63c95aef1742dd5781b1b3a40a501183e
AnmolGupta912 <pfyplannedforu@gmail.com>
2026-08-23 12:16:58 +0530

    add cloudinary utility for file uploads

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,26 @@
+import {v2 as cloudinary} from "cloudinary"
+import fs, { unlinkSync } from "fs"
+
+cloudinary.config({ 
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
+  api_key: process.env.CLOUDINARY_API_KEY, 
+  api_secret: process.env.CLOUDINARY_API_SECRET
+});
+
+const uploadFileOncloudinary = async (localPath) => {
+    try {
+        if (!localPath) return null
+
+        const response = await cloudinary.uploader.upload(localPath,{
+            resource_type: "auto"
+        })
+
+        fs.unlinkSync(localPath)
+        return response
+    } catch (error) {
+        fs.unlinkSync(localPath)
+        return null
+    }
+}
+
+export {uploadFileOncloudinary}
